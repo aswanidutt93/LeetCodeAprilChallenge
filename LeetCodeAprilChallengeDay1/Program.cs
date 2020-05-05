5b58534736e17ace2c88e0f550b89c1e013d322e
aswanidutt93 <aswanidutt93@outlook.com>
2020-05-05 21:07:03 +0530

    april day1

---
+++ LeetCodeAprilChallengeDay1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeAprilChallengeDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int[] n = new int[] { 4, 1, 2, 1, 2 };
+            var res = SingleNumber(n);
+        }
+
+        public static int SingleNumber(int[] nums)
+        {
+            Dictionary<int, int> m = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (m.ContainsKey(nums[i]))
+                {
+                    var val = m[nums[i]];
+                    m.Remove(nums[i]);
+                    m.Add(nums[i], val + 1);
+                }
+                else
+                {
+                    m.Add(nums[i], 1);
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (m[nums[i]] == 1)
+                {
+                    return nums[i];
+                }
+            }
+            return -1;
+
+        }
+    }
+}
